72d7ca1b6d00f6df701cf78996a48f84c751e691
Michael Krasnyk <michael.krasnyk@gmail.com>
2016-06-07 03:23:33 +0200

    Commented out

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include <stxxl/vector>
+//#include <stxxl/vector>
 
 int main()
 {
     std::cout << "c++ " << __cplusplus << " sizeof(void*) = " << sizeof(void*) << "\n";
 
-    stxxl::vector<unsigned> dummy_vector;
-    dummy_vector.push_back(0);
-    std::cout << dummy_vector.size() << "  " << dummy_vector[0] << "\n";
+    // stxxl::vector<unsigned> dummy_vector;
+    // dummy_vector.push_back(0);
+    // std::cout << dummy_vector.size() << "  " << dummy_vector[0] << "\n";
 
     return 0;
 }
